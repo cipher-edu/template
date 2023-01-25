9d8e0abfb05f7d401f605e65d5d598a896163729
cipher-edu <ciphereduuz@gmail.com>
2023-01-25 21:56:16 +0500

    adding django

---
+++ core/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class LandingHead(models.Model):
+    logos = models.ImageField(upload_to='logo/')
+    landing_image = models.ImageField(upload_to='images/')
+    landing_title = models.TextField(max_length=250)
