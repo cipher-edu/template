2b811453a0e2be926aa885d89452a77e539f66f0
cipher-edu <ciphereduuz@gmail.com>
2023-01-26 11:04:48 +0500

    cahnges

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ class LandingHead(models.Model):
     logos = models.ImageField(upload_to='logo/')
     landing_image = models.ImageField(upload_to='images/')
     landing_title = models.TextField(max_length=250)
+
+    # def __init__(self):
+    #     return self.landing_title
--- core/views.py
+++ core/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from .models import *
-# Create your views here.
+
 def home(request):
-    hcontent = LandingHead.objects.all()
-    context = {
-        'hcontent': hcontent
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
